9c93feaea0a3793d866df48263d7989927c8b078
José Luis Neira Núñez <joseluisnn83@gmail.com>
2020-09-16 18:52:57 +0200

    Routing implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ResourceDisplayedComponent } from './pages/Resources/resource-displayed/resource-displayed.component';
+import { ResourceListComponent } from './pages/Resources/resource-list/resource-list.component';
+import { UserEditionComponent } from './pages/Users/user-edition/user-edition.component';
+import { UserListComponent } from './pages/Users/user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path:'register', component: RegisterComponent },
+  { path:'home', component: HomeComponent }, 
+  { path:'userList', component: UserListComponent },
+  { path:'user/:id', component: UserEditionComponent },
+  { path:'resourceList', component: ResourceListComponent },
+  { path:'resource/:id', component: ResourceDisplayedComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
